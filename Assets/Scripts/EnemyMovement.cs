9b97627b79bf9d1cf5ce2fe2051d4a52a5ac846a
Leroy Korterink <leroy@mediamonks.com>
2021-01-30 20:08:52 +0100

    Add colliders to register enemy damage

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -2,49 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour {
+public class EnemyMovement : MonoBehaviour
+{
 
-    [SerializeField]
-    private int health = 3;
-    private int currentHealth = 3;
-    private bool isDead = false;
+  [SerializeField]
+  private int health = 3;
+  private int currentHealth = 3;
+  private bool isDead = false;
 
-    private Transform target;
-    private Vector3 targetPosition = Vector3.zero;
-    private Vector3 velocity = Vector3.zero;
-    public float movementSpeed = 200f;
-    public float damping = 1f;
+  private Transform target;
+  private Vector3 targetPosition = Vector3.zero;
+  private Vector3 velocity = Vector3.zero;
+  public float movementSpeed = 200f;
+  public float damping = 1f;
 
-    [SerializeField]
-    private Rigidbody body;
+  [SerializeField]
+  private Rigidbody body;
 
-    void Start () {
-        body = GetComponent<Rigidbody> ();
-        PlayerManager pm = (PlayerManager) FindObjectOfType (typeof (PlayerManager));
-        target = pm.getTransform ();
-        currentHealth = health;
+  void Start()
+  {
+    body = GetComponent<Rigidbody>();
+    PlayerManager pm = (PlayerManager)FindObjectOfType(typeof(PlayerManager));
+    target = pm.getTransform();
+    currentHealth = health;
+  }
+
+  void Update()
+  {
+    float step = movementSpeed * Time.deltaTime;
+    body.transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    body.transform.LookAt(target);
+  }
+
+  public void OnHit(int damage)
+  {
+    if (isDead)
+    {
+      return;
     }
 
-    void Update () {
-        float step = movementSpeed * Time.deltaTime;
-        body.transform.position = Vector3.MoveTowards (transform.position, target.position, step);
-        body.transform.LookAt (target);
-    }
+    currentHealth -= damage;
 
-    public void OnHit (int damage) {
-        if (!isDead) {
-            currentHealth -= damage;
-            if (currentHealth <= 0) {
-                currentHealth = 0;
-                isDead = true;
-                Destroy(gameObject);
-            }
-        }
-    }
+    if (currentHealth <= 0)
+    {
+      currentHealth = 0;
+      isDead = true;
 
-    void OnCollisionEnter(Collision collision) {
-        if (collision.gameObject.tag == "Player") {
-            OnHit(1);
-        }
+      Destroy(gameObject);
     }
+  }
+
+  void OnCollisionEnter(Collision collision)
+  {
+    if (collision.gameObject.tag == "Item")
+    {
+      OnHit(1);
+    }
+  }
 }
--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -4,27 +4,30 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject enemyPrefab;
-    [SerializeField]
-    private float spawnFrequency = 5f;
-    public int spawnLimit = 1;
-    public int spawnCount = 0;
-    void Start()
+  [SerializeField]
+  private GameObject enemyPrefab;
+  [SerializeField]
+  private float spawnFrequency = 5f;
+  public int spawnLimit = 1;
+  public int spawnCount = 0;
+  void Start()
+  {
+    StartCoroutine(SpawnEnemyTimer());
+  }
+
+  public void SpawnEnemy()
+  {
+    GameObject itemObject = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+  }
+
+  private IEnumerator SpawnEnemyTimer()
+  {
+    yield return new WaitForSeconds(spawnFrequency);
+    SpawnEnemy();
+    spawnCount++;
+    if (spawnCount < spawnLimit)
     {
-        StartCoroutine (SpawnEnemyTimer ());
+      StartCoroutine(SpawnEnemyTimer());
     }
-
-    public void SpawnEnemy() {
-        GameObject itemObject = Instantiate (enemyPrefab, transform.position, Quaternion.identity);
-    }
-
-    private IEnumerator SpawnEnemyTimer () {
-        yield return new WaitForSeconds (spawnFrequency);
-        SpawnEnemy();
-        spawnCount++;
-        if(spawnCount < spawnLimit) {
-            StartCoroutine (SpawnEnemyTimer ());
-        }
-    }
+  }
 }
