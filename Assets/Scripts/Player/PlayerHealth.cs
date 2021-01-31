e58ea8c9fb7dcdc99b1463d0d8f1bca359c5de5e
Leroy Korterink <leroy@mediamonks.com>
2021-01-31 01:16:34 +0100

    Add health bar

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
   public float health = 3f;
+  public RectTransform healthBar;
+  public float healthBarTranslateSmoothTime = 1;
   public float knockbackForce = 10f;
   public float knockbackRadius = 5f;
   public float knockbackUpwardsMomentum = 1f;
 
   private float currentHealth;
+  private float borderSize = 3f;
+  private float healthBarTranslate;
+  private float healthBarTranslateVelocity;
 
   public bool IsDead
   {
@@ -21,6 +26,20 @@ public class PlayerHealth : MonoBehaviour
     currentHealth = health;
   }
 
+  void Update()
+  {
+    var position = Vector3.zero;
+
+    position.x = Mathf.SmoothDamp(
+      healthBar.transform.localPosition.x,
+      healthBarTranslate,
+      ref healthBarTranslateVelocity,
+      healthBarTranslateSmoothTime
+    );
+
+    healthBar.transform.localPosition = position;
+  }
+
   void OnCollisionEnter(Collision collision)
   {
     if (collision.gameObject.tag != "Enemy")
@@ -28,6 +47,9 @@ public class PlayerHealth : MonoBehaviour
       return;
     }
 
-    health--;
+    currentHealth = Mathf.Max(0, currentHealth - 1);
+
+    var healthRatio = currentHealth == 0 ? 0 : currentHealth / health;
+    healthBarTranslate = -((healthBar.rect.width - borderSize) * (1 - healthRatio));
   }
 }
